fb70eee08afa406bfe182f518e06cf123dc1c1be
MOHAMMAD SHIHAB UDDIN <shihabshahidiiuc@gmail.com>
2023-06-13 14:09:52 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/academic')
+def academic():
+    return render_template('academic.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
